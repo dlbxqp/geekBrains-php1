329b1c84c3bb5e1f66bd4897402d7effbc2f60be
dlbxqp <dlbxqp@gmail.com>
2021-02-24 19:18:45 +0300

    2102241846

--- php2/lesson2/index.php
+++ php2/lesson2/index.php
@@ -16,54 +16,71 @@
 abstract class Offer{
  static $title;
  static $price;
- static $quantityOfOffersSold;
+ static $quantityORWeight;
+ static $totalCost;
 
  abstract function calculationOfTotalCost();
 
- function __construct($title, $price, $quantityOfOffersSold){
+ public function __construct($title, $price, $quantityORWeight){
   self::$title = $title;
   self::$price = $price;
-  self::$quantityOfOffersSold = $quantityOfOffersSold;
+  self::$quantityORWeight = $quantityORWeight;
  }
 }
 
 class PieceOffer extends Offer{
- public $totalCost;
-
- public function __construct($title, $price, $quantityOfOffersSold){
-  parent::$title = $title;
-  parent::$price = $price;
-  parent::$quantityOfOffersSold = $quantityOfOffersSold;
- }
-
  function calculationOfTotalCost(){
-  $this->$totalCost = parent::$price * parent::$quantityOfOffersSold;
+  parent::$totalCost = parent::$price * parent::$quantityORWeight;
 
   return "Итоговая стоимость " .
-   parent::$quantityOfOffersSold .
+   parent::$quantityORWeight .
    " штук товара с наименованием '" .
    parent::$title .
    "' по цене " .
    parent::$price .
    " руб/шт, обойдётся Вам в " .
-   $this->$totalCost .
-   " рублей."
+   parent::$totalCost .
+   " рублей.<br>"
   ;
  }
 }
 $pieceOffer = new PieceOffer('шар', 123, 9);
 echo $pieceOffer->calculationOfTotalCost();
 
-/*
 class DigitalOffer extends Offer{
- function calculationOfFinalCost(){
-  return 0;
- }
-}
+ function calculationOfTotalCost(){
+  parent::$totalCost = (parent::$price * parent::$quantityORWeight) / 2;
 
-class WightOffer extends Offer{
- function calculationOfFinalCost(){
-  return 0;
+  return "Итоговая стоимость " .
+   parent::$quantityORWeight .
+   " копий товара с наименованием '" .
+   parent::$title .
+   "' по цене " .
+   parent::$price .
+   " руб/копия, обойдётся Вам в " .
+   parent::$totalCost .
+   " рублей.<br>"
+   ;
  }
 }
-*/
+$digitalOffer = new DigitalOffer('диск', 321, 18);
+echo $digitalOffer->calculationOfTotalCost();
+
+class WeightOffer extends Offer{
+ function calculationOfTotalCost(){
+  parent::$totalCost = parent::$price * parent::$quantityORWeight;
+
+  return "Итоговая стоимость " .
+   parent::$quantityORWeight .
+   " кг товара с наименованием '" .
+   parent::$title .
+   "' по цене " .
+   parent::$price .
+   " руб/копия, обойдётся Вам в " .
+   parent::$totalCost .
+   " рублей.<br>"
+   ;
+ }
+}
+$weightOffer = new WeightOffer('картоха', 60, 10);
+echo $weightOffer->calculationOfTotalCost();
